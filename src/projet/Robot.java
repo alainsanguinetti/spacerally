8debdde2b60ae6e599e98a7432331086a1d81602
Eder <eder.jimenezs@gmail.com>
2015-03-09 17:17:14 +0100

    Eder

--- src/projet/Robot.java
+++ src/projet/Robot.java
@@ -2,4 +2,18 @@ package projet;
 
 public class Robot {
 
+	/**
+	 * Attributs
+	 */
+	int vies;
+	String orientation;
+	Carte action;
+	
+	/**
+	 * Constructeur
+	 */
+	public Robot() {
+		
+	}
+	
 }
