6479f4a76a5eeef4551634307c22a3f5d1a2c731
Alain Sanguinetti <alain@sanguinetti.eu>
2015-03-09 16:59:27 +0100

    modification, paquet a été oublié

---
+++ src/projet/Paquet.java
@@ -0,0 +1,18 @@
+package projet;
+
+import java.util.LinkedList;
+
+/**
+ * This class describes a deck of cards
+ * @author alain
+ *
+ */
+public class Paquet {
+	
+	private LinkedList<Carte> tas = new LinkedList<Carte>();
+
+	public Paquet() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
