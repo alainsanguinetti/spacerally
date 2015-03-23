3ad570b1b8357be47e1549dc286abd96219b4b36
Eder <eder.jimenezs@gmail.com>
2015-03-23 15:42:10 +0100

    cases

---
+++ src/projet/Drapeau.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package projet;
+
+/**
+ * @author eder
+ *
+ */
+public class Drapeau extends Case {
+
+	/**
+	 * 
+	 */
+	public Drapeau() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/projet/Puits.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package projet;
+
+/**
+ * @author eder
+ *
+ */
+public class Puits extends Case {
+
+	/**
+	 * 
+	 */
+	public Puits() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
